00d039ad79189255af56e829288e220c4d08d258
sourabhsapkal63 <sourabhsapkal63@gmail.com>
2025-11-28 22:07:28 +0530

    Update

--- Arrowfunction.js
+++ Arrowfunction.js
@@ -6,3 +6,6 @@ console.log(add(5, 7)); // Output: 12
 
 const square = num => num * num;
 console.log(square(10)); // Output: 100
+
+const greet = () => console.log("Hello, World!");
+greet(); // Output: Hello, World!
